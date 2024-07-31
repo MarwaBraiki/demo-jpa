d5a3f7ead062dcdb1d67d5b765ef7e507f3eb6bf
MarwaBraiki <braikimarwa91@gmail.com>
2024-07-31 16:40:34 +0200

    Premier commit

--- src/main/java/fr/digi/m052024/ConnexionJpa.java
+++ src/main/java/fr/digi/m052024/ConnexionJpa.java
@@ -1,2 +1,22 @@
-package fr.digi.m052024;public class ConnexionJpa {
+package fr.digi.m052024;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class ConnexionJpa {
+    public static void main(String[] args) {
+        // Création d'une instance d'EntityManagerFactory
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("demo");
+
+        // Création d'une instance d'EntityManager
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        // Vérification de la connexion
+        System.out.println("Connexion à la base de données réussie !");
+
+        // Fermeture des instances
+        entityManager.close();
+        entityManagerFactory.close();
+    }
 }
